51c211dce710fce54cc64232507312042059057d
SugeKnight <107584510+SAVAGE-dot@users.noreply.github.com>
2023-11-20 21:31:43 -0500

    Add files via upload

---
+++ app/views/js/ajax.js
@@ -0,0 +1,22 @@
+const formularios_ajax=document.querySelectorAll(".FormularioAjax");
+
+formularios_ajax.forEach(formularios => {
+    formularios.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        Swal.fire({
+            title: "Estas Seguro?",
+            text: "Quieres realizar la accion solicitada!",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si, realizar",
+            cancelButtonText: "No, cancelar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+            }
+        });
+    });
+
+});
